1cf3c562244da29134de8d84cd24c95541c9e85c
ca19006 <ca19006@melville-high.school.nz>
2022-11-02 09:04:55 +1300

    Just finished the circle calculation lets go next is rectangle calculation I've done 4 shapes square, circle, rectangle and parallelogram last to do is triangle

--- square_calculator.py
+++ square_calculator.py
@@ -1,6 +1,6 @@
 import math
 
-# Function to calculate the area or perimeter of a square
+# Function to calculate the area or perimeter of a square, circle, rectangle, parallelogram and triangle
 
 
 def square_calc():
@@ -46,6 +46,77 @@ def circle_calc():
             print()
 
 
+def rectangle_calc():
+    valid = False
+    while valid is False:
+        try:
+            question = input("Do you want to find the area or perimeter?: ").lower()
+            if question == "area":
+                s_side = float(input("What is the length of the short side in cm?: "))
+                l_side = float(input("What is the length of the long side in cm?: "))
+                answer = s_side * l_side
+                print("The area of your rectangle is {} cm".format(answer))
+                return answer
+            elif question == "perimeter":
+                s_side = float(input("What is the length of the short side in cm?: "))
+                l_side = float(input("What is the length of the long side in cm?: "))
+                answer = 2 * (s_side + l_side)
+                print("The perimeter of your rectangle is {} cm".format(answer))
+                return answer
+            else:
+                print("Please enter either area or perimeter")
+        except ValueError:
+            print()
+
+
+def parallelogram_calc():
+    valid = False
+    while valid is False:
+        try:
+            question = input("Do you want to find the area or perimeter: ")
+            if question == "area":
+                base = float(input("What is the length of the base in cm?: "))
+                height = float(input("What is the height of the parallelogram in cm?: "))
+                answer = base * height
+                print("The area of your parallelogram is ", answer)
+                return answer
+            elif question == "perimeter":
+                base = float(input("What is the length of the base side in cm?: "))
+                height = float(input("What is the height of the parallelogram in cm?: "))
+                answer = 2 * (base + height)
+                print("The perimeter of your parallelogram is {} cm".format(answer))
+                return answer
+            else:
+                print("Please enter either area or perimeter")
+        except ValueError:
+            print()
+
+
+# Initial variables
+valid = False
+while valid is False:
+    try:
+        question = input("What shape do you want to calculate?: ").lower()
+        if question == "square":
+            square = square_calc()
+            break
+        elif question == "circle":
+            circle = circle_calc()
+            break
+        elif question == "rectangle":
+            rectangle = rectangle_calc()
+            break
+        elif question == "parallelogram":
+            parallelogram = parallelogram_calc()
+        else:
+            print("Please enter either square, circle, rectangle or parallelogram")
+    except ValueError:
+        print()
+
+
+# Square calculations:
+# Area is Side*Side
+# Perimeter is Side*4
 
 # This is due to all sides being the same size
 
@@ -60,5 +131,5 @@ def circle_calc():
     # Note known side and known angle have to be opposite of each other and the other angle is in the first half of the equation
 
 
-# For square roots use variable = math.sqrt(variable)
+# For square roots use variable1 = math.sqrt(variable2)
 # For exponents use number/variable ** number/variable
