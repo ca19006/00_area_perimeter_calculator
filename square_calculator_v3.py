f1dd439c4e173bdc25c14d3e2c5cbab76e71d758
ca19006 <ca19006@melville-high.school.nz>
2022-11-21 11:37:28 +1300

    Due to time constraints im removing the inverse functions as I don't have time to figure out how to use the atan/asin/acah and won't have enough time to complete my assessment.

--- square_calculator_v3.py
+++ square_calculator_v3.py
@@ -1,5 +1,7 @@
 import math
 import pandas
+
+
 # Function to calculate the area or perimeter of a square, circle, rectangle, parallelogram and triangle
 
 
@@ -104,40 +106,126 @@ def triangle_calc():
                 third_side = input("Do you need a 3rd side?: ").lower()
                 if third_side == "yes":
                     question3 = input("Are you given the hypotenuse?: ").lower()
-            #
-                    # This section uses Pythagoras theorem as there are 2 sides and the user needs the 3rd side
-                    if question3 == "yes":
-                        hypotenuse = int(input("What is the length of the hypotenuse in cm?: "))
-                        opposite = int(input("What is the length of the opposite in cm?: "))
-                        adjacent = (hypotenuse**2) - (opposite**2)
-                        answer = math.sqrt(adjacent)
-                        print("The length of the adjacent is {} cm".format(answer))
-                    else:
-                        opposite = int(input("What is the length of the opposite in cm?: "))
-                        adjacent = int(input("What is the length of the adjacent in cm?: "))
-                        hypotenuse = (opposite**2) + (adjacent**2)
-                        answer = math.sqrt(hypotenuse)
-                        print("The length of the hypotenuse is {} cm".format(answer))
                     #
+                    # This section uses Pythagoras theorem as there are 2 sides and the user needs the 3rd side
+                    if question3 == "yes":  # This section is done
+                        hypotenuse = float(input("What is the length of the hypotenuse in cm?: "))
+                        opposite = float(input("What is the length of the opposite in cm?: "))
+                        _adjacent = (hypotenuse ** 2) - (opposite ** 2)
+                        adjacent = math.sqrt(_adjacent)
+                        print("The adjacent side is {} cm".format(adjacent))
+                        t_area = (0.5 * opposite) * adjacent
+                        t_perimeter = hypotenuse + adjacent + opposite
+                        history_triangle.insert(0, chosen_shape)
+                        history_hypotenuse.insert(0, hypotenuse)
+                        history_adjacent.insert(0, adjacent)
+                        history_opposite.insert(0, opposite)
+                        history_triangle_area.insert(0, t_area)
+                        history_triangle_perimeter.insert(0, t_perimeter)
+                        dataframe = pandas.DataFrame(t_shape_data)
+                        print(dataframe)
+                        print()
+                    elif question3 == "no":
+                        opposite = float(input("What is the length of the opposite in cm?: "))
+                        adjacent = float(input("What is the length of the adjacent in cm?: "))
+                        _hypotenuse = (opposite ** 2) + (adjacent ** 2)
+                        hypotenuse = math.sqrt(_hypotenuse)
+                        print("The length of the hypotenuse is {} cm".format(hypotenuse))
+                        t_area = (0.5 * opposite) * adjacent
+                        t_perimeter = hypotenuse + adjacent + opposite
+                        history_triangle.insert(0, chosen_shape)
+                        history_hypotenuse.insert(0, hypotenuse)
+                        history_adjacent.insert(0, adjacent)
+                        history_opposite.insert(0, opposite)
+                        history_triangle_area.insert(0, t_area)
+                        history_triangle_perimeter.insert(0, t_perimeter)
+                        dataframe = pandas.DataFrame(t_shape_data)
+                        print(dataframe)
+                        print()
+                    else:
+                        print("Please enter yes or no")
+                elif third_side == "no":
+                    inverse = int(input("What sides do you have? \n"
+                                        "1. Opposite and Hypotenuse \n"
+                                        "2. Adjacent and Hypotenuse \n"
+                                        "3. Opposite and Adjacent \n"
+                                        "Type Here: "))
+                    if inverse == 1:
+                        hypotenuse = float(input("What is the length of the hypotenuse in cm?: "))
+                        opposite = float(input("What is the length of the opposite in cm?: "))
+                        angle = math.degrees(math.asin(opposite / hypotenuse))
+                        print("Your missing angle is {} degrees".format(angle))
+                        _adjacent = (hypotenuse ** 2) - (opposite ** 2)
+                        adjacent = math.sqrt(_adjacent)
+                        t_perimeter = hypotenuse + opposite + adjacent
+                        t_area = 2 / adjacent * opposite
+                        adjacent_angle = math.degrees(math.acos(adjacent / hypotenuse))
+                        hypotenuse_angle = math.degrees(math.atan(opposite / adjacent))
+                        history_triangle.insert(0, chosen_shape)
+                        history_opposite.insert(0, opposite)
+                        history_hypotenuse.insert(0, hypotenuse)
+                        history_opposite_angle.insert(0, angle)
+                        history_adjacent.insert(0, adjacent)
+                        history_adjacent_angle.insert(0, adjacent_angle)
+                        history_hypotenuse_angle.insert(0, hypotenuse_angle)
+                        history_triangle_perimeter.insert(0, t_perimeter)
+                        history_triangle_area.insert(0, t_area)
+                        dataframe = pandas.DataFrame(t_shape_data)
+                        print(dataframe)
+                        angle_data_frame = pandas.DataFrame(t_angle_data)
+                        print(angle_data_frame)
+                    elif inverse == 2:
+                        hypotenuse = float(input("What is the length of the hypotenuse in cm?: "))
+                        adjacent = float(input("What is the length of the opposite in cm?: "))
+                        angle = math.degrees(math.acos(adjacent/hypotenuse))
+                        print("Your missing angle is {} degrees".format(angle))
+                        opposite = (math.tan(math.radians(angle))) * adjacent
+                        print(opposite)
+                        print(adjacent)
+                        print(hypotenuse)
+                        t_perimeter = hypotenuse + opposite + adjacent
+                        t_area = 2 / adjacent * opposite
+                        opposite_angle = math.degrees(math.asin(opposite / hypotenuse))
+                        hypotenuse_angle = math.degrees(math.atan(hypotenuse / adjacent))
+                        history_triangle.insert(0, chosen_shape)
+                        history_opposite.insert(0, opposite)
+                        history_hypotenuse.insert(0, hypotenuse)
+                        history_opposite_angle.insert(0, opposite_angle)
+                        history_adjacent.insert(0, adjacent)
+                        history_adjacent_angle.insert(0, angle)
+                        history_hypotenuse_angle.insert(0, hypotenuse_angle)
+                        history_triangle_perimeter.insert(0, t_perimeter)
+                        history_triangle_area.insert(0, t_area)
+                        dataframe = pandas.DataFrame(t_shape_data)
+                        print(dataframe)
+                        angle_data_frame = pandas.DataFrame(t_angle_data)
+                        print(angle_data_frame)
+                    elif inverse == 3:
+                        adjacent = float(input("What is the length of the adjacent in cm?: "))
+                        opposite = float(input("What is the length of the opposite in cm?: "))
+                        angle = math.degrees(math.atan(opposite / adjacent))
+                        print("Your missing angle is {} degrees".format(angle))
+                    else:
+                        print("Please enter 1, 2 or 3")
             # This Section is the SOH CAH TOA section of the triangle code
             elif two_sides == "no":
                 print("You should have 1 side and 1 angle")
                 sct = input("Do you need to use SOH / CAH / TOA?: ").lower()
                 if sct == "soh":
-                    angle = int(input("What is the angle in degrees?: "))
-                    side = int(input("What is the length of the side in cm?: "))
+                    angle = float(input("What is the angle in degrees?: "))
+                    side = float(input("What is the length of the side in cm?: "))
                     adjacent = side / math.sin(math.radians(angle))
                     print("The length of the adjacent is {} cm".format(adjacent))
                     return adjacent
                 elif sct == "cah":
-                    angle = int(input("What is the angle in degrees?: "))
-                    side = int(input("What is the length of the side in cm?: "))
+                    angle = float(input("What is the angle in degrees?: "))
+                    side = float(input("What is the length of the side in cm?: "))
                     opposite = side / math.sin(math.radians(angle))
                     print("The length of the opposite is {} cm".format(opposite))
                     return opposite
                 elif sct == "toa":
-                    angle = int(input("What is the angle in degrees?: "))
-                    side = int(input("What is the length of the side in cm?: "))
+                    angle = float(input("What is the angle in degrees?: "))
+                    side = float(input("What is the length of the side in cm?: "))
                     hypotenuse = side / math.sin(math.radians(angle))
                     print("The length of the hypotenuse {} cm".format(hypotenuse))
                     return hypotenuse
@@ -175,6 +263,15 @@ history_circle = []
 history_parallelogram = []
 history_parallelogram_area = []
 history_parallelogram_perimeter = []
+history_hypotenuse = []
+history_adjacent = []
+history_opposite = []
+history_hypotenuse_angle = []
+history_adjacent_angle = []
+history_opposite_angle = []
+history_triangle_area = []
+history_triangle_perimeter = []
+history_triangle = []
 
 sr_shape_data = {
     'Shape': history_shape,
@@ -199,11 +296,27 @@ p_shape_data = {
     'perimeter': history_parallelogram_perimeter
 }
 
+t_shape_data = {
+    'Shape': history_triangle,
+    'Hypotenuse': history_hypotenuse,
+    'Adjacent': history_adjacent,
+    'Opposite': history_opposite,
+    'Perimeter': history_triangle_perimeter,
+    'Area': history_triangle_area
+}
+
+t_angle_data = {
+    'Hypotenuse Angle': history_hypotenuse_angle,
+    'Adjacent Angle': history_adjacent_angle,
+    'Opposite Angle': history_opposite_angle,
+}
+
 shape_valid = "invalid choice"
 while shape_valid == "invalid choice":
     question = input("Do you want to calculate a shape?: ").lower()
     shape_valid = string_check(question, yes_no)
     print("You said:", shape_valid)
+    print()
 
 if shape_valid == "Yes":
     shape_choice = ""
@@ -216,6 +329,7 @@ if shape_valid == "Yes":
                              "Parallelogram \n"
                              "Type Exit to stop \n"
                              "Type Here: ").lower()
+        print()
         shape_choice = shape_choice.strip()
         if shape_choice == "exit":
             break
@@ -223,14 +337,12 @@ if shape_valid == "Yes":
         print("Shape selected: ", chosen_shape)
         if shape_choice == "square" or shape_choice == "s":
             square = square_calc()
-
         elif shape_choice == "rectangle" or shape_choice == "r":
             rectangle = rectangle_calc()
         elif shape_choice == "circle" or shape_choice == "c":
             circle = circle_calc()
         elif shape_choice == "parallelogram" or shape_choice == "p":
             parallelogram = parallelogram_calc()
-
         elif shape_choice == "triangle" or shape_choice == "t":
             triangle = triangle_calc()
         else:
